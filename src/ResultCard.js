2431a48f3868fa8e461af1f2920719659260db84
ioanungurean <inungurean@gmail.com>
2018-08-16 13:54:12 +0200

    Initial commit

--- src/ResultCard.js
+++ src/ResultCard.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
 
+const styles = {
+  root: {
+    padding: '20px',
+  },
+};
 
 class ResultCard extends Component {
   render() {
     return (
       <div className="ResultCard">
-
+        <Paper style={styles.root} elevation={1}>
+          <Typography variant="headline" component="h3">
+            This is a sheet of paper.
+          </Typography>
+          <Typography component="p">
+            Paper can be used to build surface or other elements for your application.
+          </Typography>
+        </Paper>
       </div>
     );
   }
--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
-
+import Button from '@material-ui/core/Button';
+import Input from '@material-ui/core/Input';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import IconButton from '@material-ui/core/IconButton';
+import Search from '@material-ui/icons/Search';
 
 class Searchbar extends Component {
   render() {
     return (
       <div className="Searchbar">
-        <input type="text" value="Type something..." />
-        <button>Search</button>
+        <Input
+          id="search"
+          label="Search field"
+          type="search"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="Toggle password visibility"
+              >
+                {<Search />}
+              </IconButton>
+            </InputAdornment>
+          }
+        />
+        <Button variant="contained" color="primary">Search</Button>
       </div>
     );
   }
